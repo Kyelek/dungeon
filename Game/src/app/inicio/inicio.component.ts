3ef884c01ddca12c65c3443b3d22e079fc06c068
Kyelek <fco.eugenio@hotmail.com>
2023-07-24 15:32:16 +0200

    Add componente inicio

--- Game/src/app/inicio/inicio.component.ts
+++ Game/src/app/inicio/inicio.component.ts
@@ -5,11 +5,17 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css']
 })
+
 export class InicioComponent implements OnInit {
 
   constructor(){}
 
   ngOnInit(){
-      
+
+    let audio = new Audio('../../assets/musica/medieval-inicio.mp3');
+    audio.loop = true;
+    //audio.play();     // ACTIVAR PARA QUE LA MUSICA EMPIECE
   }
+
+
 }
